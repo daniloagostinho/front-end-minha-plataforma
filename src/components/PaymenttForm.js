7e52979865eb0a9156cdbcfff9887e7063d00180
Danilo Agostinho <daniloagostinhosilva@gmail.com>
2024-11-16 17:14:31 -0300

    up

--- src/components/PaymenttForm.js
+++ src/components/PaymenttForm.js
@@ -73,7 +73,6 @@ const PaymentForm = ({ user, course, onPaymentSuccess }) => {
                     },
                 },
             };
-            await bricksBuilder.create('payment', 'paymentBrick_container', settings);
         };
 
         renderPaymentBrick();
