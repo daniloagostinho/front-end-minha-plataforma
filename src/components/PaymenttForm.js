b525efd7ae0f30c461645479691f3ee74bec7b47
Danilo Agostinho <daniloagostinhosilva@gmail.com>
2024-11-17 13:00:58 -0300

    up

--- src/components/PaymenttForm.js
+++ src/components/PaymenttForm.js
@@ -4,10 +4,10 @@ import { initMercadoPago } from '@mercadopago/sdk-react';
 const PaymentForm = ({ user, course, onPaymentSuccess }) => {
     useEffect(() => {
         // Inicializa o Mercado Pago com sua chave pública
-        initMercadoPago('APP_USR-7ccdd71d-8235-436e-b44a-bfa6aa1c05ea');
+        initMercadoPago('APP_USR-7ccdd71d-8235-436e-b44a-bfa6aa1c05ea', { locale: 'pt-BR' });
         
         const renderPaymentBrick = async () => {
-            const mp = new window.MercadoPago('APP_USR-7ccdd71d-8235-436e-b44a-bfa6aa1c05ea');
+            const mp = new window.MercadoPago('APP_USR-7ccdd71d-8235-436e-b44a-bfa6aa1c05ea', { locale: 'pt-BR' });
             const bricksBuilder = mp.bricks();
 
             const settings = {
